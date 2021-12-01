bda163bca6cce92e20b33ac81c9972850b51c07d
Allan Verde <allangreen2906@gmail.com>
2021-11-30 23:24:03 -0300

    last commit first version

---
+++ src/themes.js
@@ -0,0 +1,24 @@
+const lightTheme = {
+    color: '#3CDEC5',
+    backgroundColor: '#fff6d5',
+    button: {
+        textColor: '#fff6d5',
+        borderColor: '#151a69',
+        backgroundColor: '#151a69'
+    }
+}
+
+const darkTheme = {
+    color: '#d3d4e6',
+    backgroundColor: '#00002E',
+    button: {
+        textColor: '#F2E86D',
+        borderColor: '#F2E86D',
+        backgroundColor: '#00002E'
+    }
+}
+
+export const themes = {
+    light: lightTheme,
+    dark: darkTheme
+}
